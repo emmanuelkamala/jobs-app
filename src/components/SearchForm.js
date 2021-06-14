3a5c986105851e41db118da4d368fde6eaf7fd97
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-06-15 00:46:25 +0300

    Modify searchform component

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import {Form, Col} from 'react-bootstrap';
 
 function SearchForm({ params, onParamChange }) {
   return (
-    <div>
-      
-    </div>
+    <Form className="mb-4">
+      <Form.Row className="align-items-end">
+        <Form.Group as={Col}>
+          <Form.Label>Description</Form.Label>
+          <Form.Control onChange={onParamChange} value={params.description} name="description" type="text" />
+        </Form.Group>
+        <Form.Group as={Col} xs="auto" className="ml-2">
+          <Form.Check onChange={onParamChange} value={params.remote} name="remote" id="remote" label="Remote" type="checkbox" className="mb-2" />
+        </Form.Group>
+      </Form.Row>
+    </Form>
   )
 }
 
