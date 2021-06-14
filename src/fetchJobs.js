7ca57829f12da9116d9080ef9f0229ada9bbe6a7
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-06-14 15:39:18 +0300

    Add additional dependencies to the app

--- src/fetchJobs.js
+++ src/fetchJobs.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import { useReducer } from 'react';
 
-const fetchJobs = (params, page) => {
-    return (
-        <div>
-            
-        </div>
-    )
+const actions = {
+  MAKE_REQUEST: 'make request',
+  GET_DATA: 'get data',
+  ERROR: 'error'
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case actions.MAKE_REQUEST:
+      return { loading: true, jobs: [] }
+    case actions.GET_DATA:
+      return { ...state, loading: false, jobs: action.payload.jobs }
+    case actions.ERROR:
+      return { loading: false, error: true, error: action.payload.error, jobs: [] }
+    default:
+      return state
+  }
+}
+
+const fetchJobs = (params, page) =>{
+  const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true })
+  return (
+    {
+      jobs: [],
+      loading: false,
+      error: false,
+    }
+
+  )
 }
 
 export default fetchJobs;
